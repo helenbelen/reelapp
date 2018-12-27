9edc01a01885b79f58b7d343222ac20eec49ecbf
Helen Agha <helenbelen111@gmail.com>
2018-12-26 22:07:41 -0500

    Add Link To Browse Button

--- reelapp-source/scripts/index.js
+++ reelapp-source/scripts/index.js
@@ -3,8 +3,8 @@ var app = express();
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('video-template',{mp4source: '//vjs.zencdn.net/v/oceans.mp4', webmsource: '//vjs.zencdn.net/v/oceans.webm'});
+  res.render('video-template',{buttonsource: '/browse',mp4source: 'http://d2ua18keazfw2n.cloudfront.net/reelapp-videos-122618/video-1.mp4', webmsource: '//vjs.zencdn.net/v/oceans.webm'});
 })
 
-app.listen(3000);
-console.log("Running at port 3000");
+app.listen(3030);
+console.log("Running at port 3030");
