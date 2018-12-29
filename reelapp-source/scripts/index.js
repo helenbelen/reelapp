2141978b441a1134cd99e33bed246306f54452df
Helen Agha <helenbelen111@gmail.com>
2018-12-28 22:04:06 -0500

    Add CDN Functionality

--- reelapp-source/scripts/index.js
+++ reelapp-source/scripts/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
-  res.render('video-template',{buttonsource: '/browse',mp4source: 'http://d2ua18keazfw2n.cloudfront.net/reelapp-videos-122618/video-1.mp4', webmsource: '//vjs.zencdn.net/v/oceans.webm'});
+  res.render('video-template',{buttonsource: '/browse',mp4source: 'video-link', webmsource: '//vjs.zencdn.net/v/oceans.webm'});
 })
 
 app.listen(3030);
