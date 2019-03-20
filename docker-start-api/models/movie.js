148b409ef9cd6eb452a3fca76ed4955d7a5fe01c
Helen <31262913+helenbelen@users.noreply.github.com>
2019-03-19 22:28:40 -0500

    Create video watch api (#5)

---
+++ docker-start-api/models/movie.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
+var MovieSchema = new mongoose.Schema({
+	title: String,
+	duration: Number,
+	rating: String,
+	description: String,
+	year: Number,
+	tags: String,
+	thumb: String,
+	link: String,
+	meta:{
+		score: Number,
+	}
+
+});
+
+module.exports = mongoose.model('Movie',MovieSchema);
